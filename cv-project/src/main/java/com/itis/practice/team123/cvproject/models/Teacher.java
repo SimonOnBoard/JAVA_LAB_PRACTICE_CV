adcde32642619ae0d9c3d1fe3989f5f2fad64b08
Simon Konkov <sweetsixteen.sudo@gmail.com>
2020-07-12 16:58:38 +0300

    profile view based on role added

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/models/Teacher.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/models/Teacher.java
@@ -6,26 +6,34 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Teacher extends User {
     private String surname;
     private String name;
     private String patronymic;
     private String institution;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinTable(name = "teachers_languages",
+            joinColumns = @JoinColumn(name = "teacher_id"),
+            inverseJoinColumns = @JoinColumn(name = "language_id"))
+    private List<Language> languages;
+
 //    private List<String> positions;
 //    private List<String> educations;
 //    private List<String> academicDegrees;
-//    private List<String> languages;
+
     private String additionalInfo;
-    private String description;
+
     public Teacher(Long id, String username, String password, Role role, String email, String description) {
         super(id, username, password, role, email);
-        this.description = description;
+        this.additionalInfo = description;
     }
 
     public Teacher(Long id, String username, String password, Role role, String email) {
