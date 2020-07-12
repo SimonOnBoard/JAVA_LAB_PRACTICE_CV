5238353880489b405cde72f673b56e51a57ddab0
xiu-xiu <sh.il.almaz@gmail.com>
2020-07-12 03:53:29 +0300

    Simple Controller for students works (/works/{id})

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/repositories/WorksRepository.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/repositories/WorksRepository.java
@@ -4,6 +4,9 @@ import com.itis.practice.team123.cvproject.models.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 public interface WorksRepository extends JpaRepository<Work, Long> {
+    List<Work> getWorksByStudentId(Long studentId);
 }
