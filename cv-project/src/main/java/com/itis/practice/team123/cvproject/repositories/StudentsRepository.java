f70d80a862a45a117bccdfa577458125ea2aca65
Sumbel <sumbel.enikeeva@gmail.com>
2020-07-12 23:12:19 +0300

    search init

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/repositories/StudentsRepository.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/repositories/StudentsRepository.java
@@ -1,9 +1,16 @@
 package com.itis.practice.team123.cvproject.repositories;
 
 import com.itis.practice.team123.cvproject.models.Student;
+import com.itis.practice.team123.cvproject.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public interface StudentsRepository extends JpaRepository<Student, Long> {
+    @Query("select c.student_id from competence c where c.tag_id = :tag_id")
+    List<Student> findByTag(@Param("tag_id") Long tag_id);
 }
