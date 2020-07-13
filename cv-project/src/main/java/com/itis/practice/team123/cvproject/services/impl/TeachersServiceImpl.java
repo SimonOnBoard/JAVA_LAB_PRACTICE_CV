4bee656fbd86da61764ab79ea9e85f7ccf185cf9
Simon Konkov <sweetsixteen.sudo@gmail.com>
2020-07-13 19:59:51 +0300

    teachers service без идиотских обработок ошибок

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/services/impl/TeachersServiceImpl.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/services/impl/TeachersServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+
 @Service
 public class TeachersServiceImpl implements TeachersService {
     private TeachersRepository teachersRepository;
@@ -22,18 +23,13 @@ public class TeachersServiceImpl implements TeachersService {
 
     @Override
     public void updateTeacher(TeacherProfileInfo teacherProfileInfo, Long id) throws IllegalArgumentException {
-        try{
-            Teacher teacher = getTeacher(id);
-            teacher.setAdditionalInfo(teacherProfileInfo.getInfo());
-            teacher.setName(teacherProfileInfo.getName());
-            teacher.setPatronymic(teacherProfileInfo.getPatronymic());
-            teacher.setInstitution(teacherProfileInfo.getInstitution());
-            teacher.setSurname(teacherProfileInfo.getSurname());
-            teachersRepository.saveAndFlush(teacher);
-        } catch (IllegalArgumentException e){
-            // TODO: 12.07.2020 обработать нормально
-            throw new IllegalArgumentException(e);
-        }
+        Teacher teacher = getTeacher(id);
+        teacher.setAdditionalInfo(teacherProfileInfo.getInfo());
+        teacher.setName(teacherProfileInfo.getName());
+        teacher.setPatronymic(teacherProfileInfo.getPatronymic());
+        teacher.setInstitution(teacherProfileInfo.getInstitution());
+        teacher.setSurname(teacherProfileInfo.getSurname());
+        teachersRepository.saveAndFlush(teacher);
     }
 
     @Override
@@ -43,36 +39,28 @@ public class TeachersServiceImpl implements TeachersService {
 
     @Override
     @Transactional
-    public void addLanguage(Long id, Language language) {
-        try {
-            Teacher teacher = this.getTeacher(id);
-            Language language1 = languageService.initializeLanguage(language);
-            removeIfExists(teacher.getLanguages(), language1);
-            teacher.getLanguages().add(language1);
-        }catch (IllegalArgumentException e){
-            // TODO: 12.07.2020 обработать нормально
-            throw new IllegalArgumentException(e);
-        }
+    public void addLanguage(Long id, Language language) throws IllegalArgumentException {
+        Teacher teacher = this.getTeacher(id);
+        Language language1 = languageService.initializeLanguage(language);
+        removeIfExists(teacher.getLanguages(), language1);
+        teacher.getLanguages().add(language1);
     }
 
     @Override
     @Transactional
     public void removeLanguage(Long id, Long language) throws IllegalArgumentException {
-        try {
-            Teacher teacher = this.getTeacher(id);
-            Language language1 = languageService.getLanguage(language);
-            teacher.getLanguages().remove(language1);
-        }catch (IllegalArgumentException e){
-            // TODO: 12.07.2020 обработать нормально
-            throw new IllegalArgumentException(e);
-        }
+        Teacher teacher = this.getTeacher(id);
+        Language language1 = languageService.getLanguage(language);
+        teacher.getLanguages().remove(language1);
+        teachersRepository.saveAndFlush(teacher);
+
     }
 
     private boolean removeIfExists(List<Language> languages, Language languageToRemove) {
         boolean result = false;
-        for(Language language: languages){
+        for (Language language : languages) {
             //подумать над случаями апперкейся
-            if(language.getLanguage().toUpperCase().equals(languageToRemove.getLanguage().toUpperCase())){
+            if (language.getLanguage().toUpperCase().equals(languageToRemove.getLanguage().toUpperCase())) {
                 languages.remove(languageToRemove);
                 result = true;
                 break;
