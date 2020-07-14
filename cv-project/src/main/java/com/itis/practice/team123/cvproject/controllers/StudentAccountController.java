f69405bf9d84f263576ff2fbd46f5b0725cee2a7
Renat <renat.blinov@yandex.ru>
2020-07-14 22:24:59 +0300

    Changed student profile and account controller

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentAccountController.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentAccountController.java
@@ -3,52 +3,48 @@ package com.itis.practice.team123.cvproject.controllers;
 import com.itis.practice.team123.cvproject.dto.*;
 import com.itis.practice.team123.cvproject.security.details.UserDetailsImpl;
 import com.itis.practice.team123.cvproject.services.interfaces.StudentsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/studentAccount/")
+@RequiredArgsConstructor
 public class StudentAccountController {
 
     private final StudentsService studentsService;
 
-    public StudentAccountController(StudentsService studentsService) {
-        this.studentsService = studentsService;
-    }
-
-    @PutMapping("/info/update")
+    @PutMapping(value = {"/studentAccount/info/update", "/api/studentAccount/info/update"})
     public ResponseEntity<?> updateBaseInfo(@RequestBody StudentForm studentForm,
                                             @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
         studentsService.updateStudentBaseInfo(studentForm, userDetails.getUserId());
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/competences/update")
+    @PutMapping(value = {"/studentAccount/competences/update", "/api/studentAccount/competences/update"})
     public ResponseEntity<?> updateCompetencesInfo(@RequestBody TagDto tagDto,
                                                    @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
         studentsService.updateStudentCompetencesInfo(tagDto, userDetails.getUserId());
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/languages/update")
+    @PutMapping(value = {"/studentAccount/languages/update", "/api/studentAccount/languages/update"})
     public ResponseEntity<?> updateLanguagesInfo(@RequestBody LanguageDto languageDto,
                                                  @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
         studentsService.updateStudentLanguagesInfo(languageDto, userDetails.getUserId());
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/education/update")
+    @PutMapping(value = {"/studentAccount/education/update", "/api/studentAccount/education/update"})
     public ResponseEntity<?> updateEducationInfo(@RequestBody EducationDto educationDto,
                                                  @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
         studentsService.updateStudentEducationInfo(educationDto, userDetails.getUserId());
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/certificates/update")
+    @PutMapping(value = {"/studentAccount/certificates/update", "/api/studentAccount/certificates/update"})
     public ResponseEntity<?> updateCertificatesInfo(@RequestBody CertificateDto certificateDto,
                                                     @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
         studentsService.updateStudentCertificatesInfo(certificateDto, userDetails.getUserId());
