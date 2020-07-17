357b27619e055a74e52e7060248b737ec4e259a7
xiu-xiu <sh.il.almaz@gmail.com>
2020-07-17 16:29:48 +0300

    bearer token for studies API

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudiesIntegrationAPI.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudiesIntegrationAPI.java
@@ -1,5 +1,6 @@
 package com.itis.practice.team123.cvproject.controllers;
 
+import com.itis.practice.team123.cvproject.dto.BearerDto;
 import com.itis.practice.team123.cvproject.dto.WorkDto;
 import com.itis.practice.team123.cvproject.models.Student;
 import com.itis.practice.team123.cvproject.models.Tag;
@@ -7,6 +8,7 @@ import com.itis.practice.team123.cvproject.models.Teacher;
 import com.itis.practice.team123.cvproject.models.Work;
 import com.itis.practice.team123.cvproject.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudiesIntegrationAPI {
+    @Value("${bearer.token}")
+    private String bearerToken;
+
     private TeachersRepository teachersRepository;
     private StudentsRepository studentsRepository;
     private TagsRepository tagsRepository;
@@ -33,7 +38,11 @@ public class StudiesIntegrationAPI {
 
     @PreAuthorize("permitAll()")
     @PostMapping("/works")
-    public ResponseEntity<?> addWork(@RequestBody WorkDto workDto) {
+    public ResponseEntity<?> addWork(@RequestBody BearerDto bearerDto) {
+        if (!bearerDto.getBearer().equals(bearerToken)) {
+            return ResponseEntity.status(403).build();
+        }
+        WorkDto workDto = bearerDto.getWorkDto();
         Teacher teacher = teachersRepository.getOne(workDto.getTeacherId());
         Student student = studentsRepository.getOne(workDto.getStudentId());
         List<Tag> tags = tagsRepository.findAllByNameIn(workDto.getTags());
