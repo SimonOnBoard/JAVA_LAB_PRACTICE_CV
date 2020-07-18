9d39d68f63b559bd72e2233608d6b496b88f245c
xiu-xiu <sh.il.almaz@gmail.com>
2020-07-18 08:25:46 +0300

    studentNewProject.ftlh, studentProjects.ftlh

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentProjectsController.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentProjectsController.java
@@ -1,55 +1,30 @@
 package com.itis.practice.team123.cvproject.controllers;
 
-import com.itis.practice.team123.cvproject.dto.ProjectCommentDto;
 import com.itis.practice.team123.cvproject.dto.ProjectDto;
 import com.itis.practice.team123.cvproject.security.details.UserDetailsImpl;
-import com.itis.practice.team123.cvproject.services.interfaces.ProjectsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
+@Controller
 public class StudentProjectsController {
-
-    private final ProjectsService projectsService;
-
     @PreAuthorize("hasAnyRole('TEACHER', 'COMPANY', 'ADMIN')")
-    @GetMapping("/api/students/{id}/projects")
-    public ResponseEntity<List<ProjectDto>> getStudentProjects(@PathVariable("id") Long id) {
-        return ResponseEntity.ok().body(ProjectDto.from(projectsService.getAllProjects(id)));
-    }
-
-    @PreAuthorize("hasAnyRole('STUDENT', 'TEACHER', 'COMPANY', 'ADMIN')")
-    @GetMapping("/projects/{projectId}")
-    public ResponseEntity<ProjectDto> getProject(@PathVariable("projectId") Long projectId) {
-        return ResponseEntity.ok().body(ProjectDto.from(projectsService.getProjectById(projectId)));
-    }
-
-    @PreAuthorize("hasAnyRole('STUDENT')")
-    @GetMapping("/api/studentAccount/projects")
-    public ResponseEntity<List<ProjectDto>> getProjects(@AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
-        return ResponseEntity.ok().body(ProjectDto.from(projectsService.getAllProjects(userDetails.getUserId())));
+    @GetMapping("students/{id}/projects")
+    public String getProjectsPage(@PathVariable("id") Long userId,
+                                                      @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
+        if (userDetails.getUserId().equals(userId)) {
+            return "redirect:/studentAccount/projects";
+        } else {
+            return "studentProjects";
+        }
     }
 
     @PreAuthorize("hasAnyRole('STUDENT')")
-    @PostMapping("/projects/newProject")
-    public ResponseEntity<?> addNewProject(@RequestBody ProjectDto projectDto,
-                                           @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
-        projectsService.addNewProject(projectDto, userDetails.getUser());
-        return ResponseEntity.ok().build();
-    }
-
-    @PreAuthorize("hasAnyRole('TEACHER')")
-    @PostMapping("/projects/{projectId}/newComment")
-    public ResponseEntity<?> commentProject(@RequestBody ProjectCommentDto projectCommentDto,
-                                            @PathVariable("projectId") Long projectId,
-                                            @AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
-        projectsService.commentProject(projectCommentDto, projectId, userDetails.getUser() );
-        return ResponseEntity.ok().build();
+    @GetMapping("studentAccount/projects")
+    public String getProjectsPage(@AuthenticationPrincipal UserDetailsImpl<?> userDetails) {
+        return "studentNewProject";
     }
 }
