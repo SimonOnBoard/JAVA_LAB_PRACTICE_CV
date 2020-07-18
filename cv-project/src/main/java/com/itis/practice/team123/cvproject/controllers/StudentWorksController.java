9b7be0683be0a5667a667f73c0497e88c694cd63
xiu-xiu <sh.il.almaz@gmail.com>
2020-07-18 04:15:12 +0300

    simon asked

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentWorksController.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentWorksController.java
@@ -1,8 +1,11 @@
 package com.itis.practice.team123.cvproject.controllers;
 
 import com.itis.practice.team123.cvproject.dto.WeightedWorkDto;
+import com.itis.practice.team123.cvproject.models.Student;
 import com.itis.practice.team123.cvproject.models.Work;
+import com.itis.practice.team123.cvproject.security.details.UserDetailsImpl;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentWorksRestController.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudentWorksRestController.java
@@ -1,10 +1,13 @@
 package com.itis.practice.team123.cvproject.controllers;
 
 import com.itis.practice.team123.cvproject.dto.WeightedWorkDto;
+import com.itis.practice.team123.cvproject.models.Student;
 import com.itis.practice.team123.cvproject.models.Work;
 import com.itis.practice.team123.cvproject.repositories.WorksRepository;
+import com.itis.practice.team123.cvproject.security.details.UserDetailsImpl;
 import com.itis.practice.team123.cvproject.services.interfaces.WeightsAssigner;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +25,8 @@ public class StudentWorksRestController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/api/works/{id}")
-    public List<WeightedWorkDto> getStudentsWorks(@PathVariable Long id, ) {
+    @GetMapping("/load/works/{id}")
+    public List<WeightedWorkDto> getStudentsWorks(@PathVariable Long id) {
         List<Work> works = worksRepository.getWorksByStudentId(id);
         return weightsAssigner.assignWorkWeights(works);
     }
