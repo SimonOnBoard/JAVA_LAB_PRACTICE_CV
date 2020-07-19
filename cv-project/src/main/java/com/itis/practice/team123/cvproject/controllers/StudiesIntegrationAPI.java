06ac40559effe3a5d039e5bd7d73233c1953bec3
xiu-xiu <sh.il.almaz@gmail.com>
2020-07-19 19:53:50 +0300

    addWorkByTeacher

--- cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudiesIntegrationAPI.java
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/controllers/StudiesIntegrationAPI.java
@@ -1,16 +1,21 @@
 package com.itis.practice.team123.cvproject.controllers;
 
 import com.itis.practice.team123.cvproject.dto.BearerDto;
+import com.itis.practice.team123.cvproject.dto.TeachersWorkDto;
 import com.itis.practice.team123.cvproject.dto.WorkDto;
 import com.itis.practice.team123.cvproject.models.Student;
 import com.itis.practice.team123.cvproject.models.Tag;
 import com.itis.practice.team123.cvproject.models.Teacher;
 import com.itis.practice.team123.cvproject.models.Work;
 import com.itis.practice.team123.cvproject.repositories.*;
+import com.itis.practice.team123.cvproject.security.details.UserDetailsImpl;
+import com.itis.practice.team123.cvproject.services.impl.WorksServiceImpl;
+import com.itis.practice.team123.cvproject.services.interfaces.WorksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,37 +28,39 @@ import java.util.List;
 public class StudiesIntegrationAPI {
     @Value("${bearer.token}")
     private String bearerToken;
+    private final WorksService worksService;
 
-    private TeachersRepository teachersRepository;
-    private StudentsRepository studentsRepository;
-    private TagsRepository tagsRepository;
-    private WorksRepository worksRepository;
+    public StudiesIntegrationAPI(WorksService worksService) {
+        this.worksService = worksService;
+    }
 
-    public StudiesIntegrationAPI(TeachersRepository teachersRepository, StudentsRepository studentsRepository, TagsRepository tagsRepository, WorksRepository worksRepository) {
-        this.teachersRepository = teachersRepository;
-        this.studentsRepository = studentsRepository;
-        this.tagsRepository = tagsRepository;
-        this.worksRepository = worksRepository;
+    @PreAuthorize("hasRole('TEACHER')")
+    @PostMapping("/addWorkByTeacher")
+    public ResponseEntity<?> addWorkByTeacher(@AuthenticationPrincipal UserDetailsImpl<?> userDetails,
+                                              @RequestBody TeachersWorkDto teachersWorkDto) {
+        WorkDto workDto = WorkDto.builder()
+                .teacherId(userDetails.getUserId())
+                .studentId(teachersWorkDto.getStudentId())
+                .tags(teachersWorkDto.getTags())
+                .title(teachersWorkDto.getTitle())
+                .description(teachersWorkDto.getDescription())
+                .build();
+        try {
+            worksService.addWork(workDto);
+            return ResponseEntity.ok("Work successfully exported");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PreAuthorize("permitAll()")
-    @PostMapping("/works")
+    @PostMapping("/addWork")
     public ResponseEntity<?> addWork(@RequestBody BearerDto bearerDto) {
         if (!bearerDto.getBearer().equals(bearerToken)) {
             return ResponseEntity.status(403).build();
         }
-        WorkDto workDto = bearerDto.getWorkDto();
-        Teacher teacher = teachersRepository.getOne(workDto.getTeacherId());
-        Student student = studentsRepository.getOne(workDto.getStudentId());
-        List<Tag> tags = tagsRepository.findAllByNameIn(workDto.getTags());
-        Work work = Work.builder()
-                .description(workDto.getDescription())
-                .title(workDto.getTitle())
-                .student(student)
-                .teacher(teacher)
-                .tags(tags).build();
         try {
-            worksRepository.save(work);
+            worksService.addWork(bearerDto.getWorkDto());
             return ResponseEntity.ok("Work successfully exported");
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().build();
---
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/services/impl/WorksServiceImpl.java
@@ -0,0 +1,40 @@
+package com.itis.practice.team123.cvproject.services.impl;
+
+import com.itis.practice.team123.cvproject.dto.WorkDto;
+import com.itis.practice.team123.cvproject.models.Student;
+import com.itis.practice.team123.cvproject.models.Tag;
+import com.itis.practice.team123.cvproject.models.Teacher;
+import com.itis.practice.team123.cvproject.models.Work;
+import com.itis.practice.team123.cvproject.repositories.StudentsRepository;
+import com.itis.practice.team123.cvproject.repositories.TagsRepository;
+import com.itis.practice.team123.cvproject.repositories.TeachersRepository;
+import com.itis.practice.team123.cvproject.repositories.WorksRepository;
+import com.itis.practice.team123.cvproject.services.interfaces.WorksService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class WorksServiceImpl implements WorksService {
+    private final TeachersRepository teachersRepository;
+    private final StudentsRepository studentsRepository;
+    private final TagsRepository tagsRepository;
+    private final WorksRepository worksRepository;
+
+    @Override
+    public void addWork(WorkDto workDto) throws RuntimeException {
+        Teacher teacher = teachersRepository.getOne(workDto.getTeacherId());
+        Student student = studentsRepository.getOne(workDto.getStudentId());
+        List<Tag> tags = tagsRepository.findAllByNameIn(workDto.getTags());
+        Work work = Work.builder()
+                .description(workDto.getDescription())
+                .title(workDto.getTitle())
+                .student(student)
+                .teacher(teacher)
+                .tags(tags).build();
+        worksRepository.save(work);
+    }
+}
