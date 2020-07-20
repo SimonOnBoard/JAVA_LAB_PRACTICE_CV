aeff257e0fabf02396ac161a4e9c6bf108186f8c
xiu-xiu <sh.il.almaz@gmail.com>
2020-07-20 03:04:24 +0300

    vacancies canevas

---
+++ cv-project/src/main/java/com/itis/practice/team123/cvproject/services/impl/VacanciesServiceImpl.java
@@ -0,0 +1,6 @@
+package com.itis.practice.team123.cvproject.services.impl;
+
+import com.itis.practice.team123.cvproject.services.interfaces.VacanciesService;
+
+public class VacanciesServiceImpl implements VacanciesService {
+}
